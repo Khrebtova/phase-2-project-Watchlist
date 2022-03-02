8dd9fae8cacb4f1d0ed91baa5384eedc930dec93
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-02 14:28:18 -0800

    create db.json file with shows data

--- src/components/NewMovieForm.jsx
+++ src/components/NewMovieForm.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const NewMovieForm = () => {
-    
-    const handleSubmit =()=>{
-       console.log("submit") 
+    const [newShow, setNewShow] = useState({  
+        "title": "",
+        "image": "",
+        "type": "",
+    })
+  
+    function handleInputChange(e){
+      const key = e.target.name;
+      const value = e.target.value
+      const formData = {...newShow, [key]: value}
+      setNewShow(formData)    
     }
 
-    const handleInputChange = (e) => {
-        console.log(e.target.vale)
+    const handleSubmit =(e)=>{
+       e.preventDefault()
+       console.log("submit", newShow) 
     }
+
+   
   return (
     <div className="new-movie-form">
       <h2> Add something new </h2>
-      <form >
-        <input type="text" name="title" placeholder="movie title" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <label>Movie<input type="radio" name="type" value="movie" /></label>
-        <label>TV Series<input type="radio" name="type" value="TVseries" /></label>
+      <form onSubmit={handleSubmit} >
+        <input onChange={handleInputChange} type="text" name="title" placeholder="movie title" />
+        <input onChange={handleInputChange} type="text" name="image" placeholder="Image URL" />
+        <label>Movie<input onChange={handleInputChange} type="radio" name="type" value="movie" /></label>
+        <label>TV Series<input onChange={handleInputChange} type="radio" name="type" value="TVseries" /></label>
         <button type="submit">Add to Watchlist</button>
       </form>
     </div>
